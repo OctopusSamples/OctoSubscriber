f3c4a29bf5abef5c69ef9bcc9e2cff555357da85
BobJWalker <bob.walker@octopus.com>
2021-10-22 16:31:46 -0500

    Fixing up a literal value

--- aws/infrastructure/main.tf
+++ aws/infrastructure/main.tf
@@ -9,7 +9,7 @@ terraform {
   backend "s3" {
     bucket = "${var.backend_bucket}"
     key = "${var.backend_key}"
-    region = "${var.backend_region}
+    region = "${var.backend_region}"
   }
 }
 
